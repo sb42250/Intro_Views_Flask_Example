f124060e395c2dc0b3e8a1ec8a1203a2a7864620
jczetta <jczetta@gmail.com>
2017-11-28 22:42:13 -0500

    initial files

---
+++ views_app.py
@@ -0,0 +1,34 @@
+# Import statements necessary
+from flask import Flask, render_template
+from flask_script import Manager
+
+# Set up application
+app = Flask(__name__)
+
+manager = Manager(app)
+
+# Routes
+
+@app.route('/')
+def hello_world():
+    return '<h1>Hello World!</h1>'
+
+@app.route('/user/<yourname>')
+def hello_name(yourname):
+    return '<h1>Hello {}</h1>'.format(yourname)
+
+@app.route('/showvalues/<name>')
+def basic_values_list(name):
+    lst = ["hello","goodbye","tomorrow","many","words","jabberwocky"]
+    if len(name) > 3:
+        longname = name
+        shortname = None
+    else:
+        longname = None
+        shortname = name
+    return render_template('values.html',word_list=lst,long_name=longname,short_name=shortname)
+
+
+
+if __name__ == '__main__':
+    manager.run() # Runs the flask server in a special way that makes it nice to debug
